5e3b5d24ed83a2ea61cd87d0e62571ada808dfa2
Priydarshini Karanth <kpriya1203@gmail.com>
2024-09-25 15:41:24 +0530

    creating pages about and contact

---
+++ 01-nextjs-tutorial-typescript/app/about/page.tsx
@@ -0,0 +1,9 @@
+function AboutPage() {
+  return (
+    <div>
+      <h1 className="text-5xl text-center">About Page</h1>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ 01-nextjs-tutorial-typescript/app/contact/page.tsx
@@ -0,0 +1,9 @@
+function ContactPage() {
+  return (
+    <div>
+      <h1 className="text-5xl text-center">Contact Page</h1>
+    </div>
+  );
+}
+
+export default ContactPage;
