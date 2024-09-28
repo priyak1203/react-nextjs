41342aca413162139f9e1509ce1848649be1406c
Priydarshini Karanth <kpriya1203@gmail.com>
2024-09-28 20:28:50 +0530

    adding fonts

--- 01-nextjs-tutorial-typescript/app/layout.tsx
+++ 01-nextjs-tutorial-typescript/app/layout.tsx
@@ -1,5 +1,12 @@
 import Navbar from '@/components/Navbar';
 import './globals.css';
+import { Inter, Inconsolata, Roboto } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+const inconsolata = Inconsolata({ subsets: ['latin'] });
+
+const roboto = Roboto({ subsets: ['latin'], weight: ['500'] });
 
 export default function RootLayout({
   children,
@@ -8,9 +15,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         <Navbar />
-        <main className="max-w-5xl mx-auto py-10">{children}</main>
+        <main className="max-w-5xl mx-auto py-10">
+          {children}
+          <div className={`py-5 ${inconsolata.className}`}>
+            <h3>Inconsolata Font</h3>
+            <p>This section contains inconsolata font</p>
+          </div>
+          <div className={roboto.className}>
+            <h3>Roboto Font</h3>
+            <p>This section contains roboto font</p>
+          </div>
+        </main>
       </body>
     </html>
   );
