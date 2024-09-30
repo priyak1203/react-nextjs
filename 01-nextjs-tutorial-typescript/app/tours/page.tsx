511963b921adcc1e9fc3b7293314db6b50ae1664
Priydarshini Karanth <kpriya1203@gmail.com>
2024-09-30 20:30:05 +0530

    fetching data in the server component

---
+++ 01-nextjs-tutorial-typescript/app/tours/page.tsx
@@ -0,0 +1,30 @@
+const url = 'https://www.course-api.com/react-tours-project';
+
+type Tour = {
+  id: string;
+  name: string;
+  info: string;
+  image: string;
+  price: string;
+};
+
+async function ToursPage() {
+  const response = await fetch(url);
+  const data: Tour[] = await response.json();
+  console.log(data);
+
+  return (
+    <section>
+      <h1 className="text-3xl mb-4">Tours</h1>
+      {data.map((tour) => {
+        return (
+          <h2 key={tour.id} className="mt-2">
+            {tour.name}
+          </h2>
+        );
+      })}
+    </section>
+  );
+}
+
+export default ToursPage;
