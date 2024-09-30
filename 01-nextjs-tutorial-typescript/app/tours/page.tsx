5ccd45ecd6c93340b9a040855a90d15be343f04d
Priydarshini Karanth <kpriya1203@gmail.com>
2024-09-30 21:56:59 +0530

    fetch data as a seperate function

--- 01-nextjs-tutorial-typescript/app/tours/page.tsx
+++ 01-nextjs-tutorial-typescript/app/tours/page.tsx
@@ -8,15 +8,19 @@ type Tour = {
   price: string;
 };
 
-async function ToursPage() {
+const fetchTours = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   const response = await fetch(url);
   const data: Tour[] = await response.json();
-  console.log(data);
+  return data;
+};
 
+async function ToursPage() {
+  const tours = await fetchTours();
   return (
     <section>
       <h1 className="text-3xl mb-4">Tours</h1>
-      {data.map((tour) => {
+      {tours.map((tour) => {
         return (
           <h2 key={tour.id} className="mt-2">
             {tour.name}
