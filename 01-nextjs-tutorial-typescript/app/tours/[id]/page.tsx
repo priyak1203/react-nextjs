bd7438e76ad4bfb1934e09bc0cf04be8218d5b2b
Priydarshini Karanth <kpriya1203@gmail.com>
2024-10-03 20:30:07 +0530

    local image of original size

--- 01-nextjs-tutorial-typescript/app/tours/[id]/page.tsx
+++ 01-nextjs-tutorial-typescript/app/tours/[id]/page.tsx
@@ -1,8 +1,19 @@
+import mountainImg from '@/images/mountains.jpg';
+import Image from 'next/image';
+
 function page({ params }: { params: { id: string } }) {
   console.log(params);
+
   return (
     <div>
       <h1 className="text-4xl">ID: {params.id}</h1>
+      <section className="mt-4 flex gap-x-4">
+        {/* local image */}
+        <div>
+          <Image src={mountainImg} alt="mountain" />
+          <h2>Local Image</h2>
+        </div>
+      </section>
     </div>
   );
 }
