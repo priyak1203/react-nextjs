620568ad8cfd254e23f66f98c93670e0d7f2e232
Priydarshini Karanth <kpriya1203@gmail.com>
2024-10-05 19:10:47 +0530

    accessing route parameters

--- 01-nextjs-tutorial-typescript/app/(dashboard)/auth/[...sign-in]/page.tsx
+++ 01-nextjs-tutorial-typescript/app/(dashboard)/auth/[...sign-in]/page.tsx
@@ -1,7 +1,10 @@
 function SignInPage({ params }: { params: { 'sign-in': string[] } }) {
   console.log(params);
+  console.log(params['sign-in']);
+  console.log(params['sign-in'][0]);
+  console.log(params['sign-in'][1]);
 
-  return <div>SignInPage</div>;
+  return <div>SignInPage - {params['sign-in'][0]}</div>;
 }
 
 export default SignInPage;
