bf1a2e14370e95cceaabc0f03f0672b95e6da29f
Priydarshini Karanth <kpriya1203@gmail.com>
2024-10-07 23:30:22 +0530

    save user function in server action file

--- 01-nextjs-tutorial-typescript/utils/actions.ts
+++ 01-nextjs-tutorial-typescript/utils/actions.ts
@@ -1,10 +1,29 @@
 'use server';
 
+import { writeFile, readFile } from 'fs/promises';
+
+type User = {
+  id: string;
+  firstName: string;
+  lastName: string;
+};
+
+export const fetchUsers = async (): Promise<User[]> => {
+  const result = await readFile('users.json', { encoding: 'utf-8' });
+  const users = result ? JSON.parse(result) : [];
+  return users;
+};
+
+const saveUser = async (user: User) => {
+  const users = await fetchUsers();
+  users.push(user);
+  await writeFile('users.json', JSON.stringify(users));
+};
+
 export const createUser = async (formData: FormData) => {
   'use server';
   const firstName = formData.get('firstName') as string;
   const lastName = formData.get('lastName') as string;
-  const rawData = Object.fromEntries(formData);
-  console.log(rawData);
-  console.log({ firstName, lastName });
+  const newUser: User = { id: Date.now().toString(), firstName, lastName };
+  await saveUser(newUser);
 };
