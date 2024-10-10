efcd93b655dc0b537b7c2e32967c9b58b0d3eb7a
Priydarshini Karanth <kpriya1203@gmail.com>
2024-10-10 21:36:20 +0530

    passing data with bind method

--- 01-nextjs-tutorial-typescript/components/DeleteButton.tsx
+++ 01-nextjs-tutorial-typescript/components/DeleteButton.tsx
@@ -1,9 +1,11 @@
-import { deleteUser } from '@/utils/actions';
+import { deleteUser, removeUser } from '@/utils/actions';
 
 function DeleteButton({ id }: { id: string }) {
+  const removeUserWithId = removeUser.bind(null, id);
+
   return (
-    <form action={deleteUser}>
-      <input type="hidden" name="id" value={id} />
+    <form action={removeUserWithId}>
+      <input type="hidden" name="name" value="shakeAndBake" />
       <button
         type="submit"
         className="bg-red-500 text-white text-xs rounded p-2 capitalize"
