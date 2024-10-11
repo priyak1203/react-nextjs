9bafe7be2eb9266e7a7123e3212362a0ce62ca50
Priydarshini Karanth <kpriya1203@gmail.com>
2024-10-11 15:18:11 +0530

    accessing request object and params in route handlers

--- 01-nextjs-tutorial-typescript/app/api/users/route.ts
+++ 01-nextjs-tutorial-typescript/app/api/users/route.ts
@@ -1,6 +1,15 @@
 import { fetchUsers } from '@/utils/actions';
 
-export const GET = async () => {
+export const GET = async (req: Request) => {
+  console.log(req.url);
+  console.log(new URL(req.url));
+
+  const { searchParams } = new URL(req.url);
+  console.log(searchParams);
+
+  const id = searchParams.get('id');
+  console.log(id);
+
   const users = await fetchUsers();
   return Response.json({ users });
 };
