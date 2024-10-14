9b679b6037b686ad4797a444f20da8cdfb3c3493
Priydarshini Karanth <kpriya1203@gmail.com>
2024-10-14 15:34:06 +0530

    cartbutton and navsearch components setup

--- 02-store-project/components/navbar/CartButton.tsx
+++ 02-store-project/components/navbar/CartButton.tsx
@@ -1,5 +1,26 @@
+import Link from 'next/link';
+import { Button } from '../ui/button';
+import { LuShoppingCart } from 'react-icons/lu';
+
 function CartButton() {
-  return <div>CartButton</div>;
+  // temp
+  const numItemsInCart = 9;
+
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      asChild
+      className="relative flex justify-center items-center"
+    >
+      <Link href="/cart">
+        <LuShoppingCart />
+        <span className="absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex justify-center items-center text-xs ">
+          {numItemsInCart}
+        </span>
+      </Link>
+    </Button>
+  );
 }
 
 export default CartButton;
