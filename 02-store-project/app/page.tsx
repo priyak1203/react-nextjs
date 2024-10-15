6ab143216508023852e98181ce76d7840785e6ed
Priydarshini Karanth <kpriya1203@gmail.com>
2024-10-15 15:27:58 +0530

    shadcn changing the theme

--- 02-store-project/app/page.tsx
+++ 02-store-project/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 function HomePage() {
   return (
     <div>
-      <h1 className="text-3xl">Home Page</h1>
+      <h1 className="text-3xl text-muted-foreground">Home Page</h1>
       <Button variant="outline" size="lg" className="capitalize m-8">
         click me
       </Button>
