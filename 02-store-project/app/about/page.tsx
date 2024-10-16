164480eb070ed9f306850934d56e4241e087e97d
Priydarshini Karanth <kpriya1203@gmail.com>
2024-10-16 19:03:14 +0530

    product model added in prisma schema

--- 02-store-project/app/about/page.tsx
+++ 02-store-project/app/about/page.tsx
@@ -1,20 +1,7 @@
-import db from '@/utils/db';
-
-async function AboutPage() {
-  const profile = await db.testProfile.create({
-    data: {
-      name: 'random name',
-    },
-  });
-
-  const users = await db.testProfile.findMany();
-
+function AboutPage() {
   return (
     <div>
       <h2 className="text-2xl">About Page</h2>
-      {users.map((user) => {
-        return <h3 key={user.id}>{user.name}</h3>;
-      })}
     </div>
   );
 }
