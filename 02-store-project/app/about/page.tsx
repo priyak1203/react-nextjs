2733d29f0a064ffdaa95f19cc8c12e02c77e73a7
Priydarshini Karanth <kpriya1203@gmail.com>
2024-10-16 18:55:25 +0530

    creating the entry and fetching the entries from db

--- 02-store-project/app/about/page.tsx
+++ 02-store-project/app/about/page.tsx
@@ -1,7 +1,20 @@
-function AboutPage() {
+import db from '@/utils/db';
+
+async function AboutPage() {
+  const profile = await db.testProfile.create({
+    data: {
+      name: 'random name',
+    },
+  });
+
+  const users = await db.testProfile.findMany();
+
   return (
     <div>
       <h2 className="text-2xl">About Page</h2>
+      {users.map((user) => {
+        return <h3 key={user.id}>{user.name}</h3>;
+      })}
     </div>
   );
 }
