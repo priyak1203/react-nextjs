a1f8dde467f1ac7bfd4740472fd6cc826f25c7a1
Priydarshini Karanth <kpriya1203@gmail.com>
2024-10-17 12:29:15 +0530

    featured products component structure with fetching products

--- 02-store-project/components/home/FeaturedProducts.tsx
+++ 02-store-project/components/home/FeaturedProducts.tsx
@@ -1,9 +1,17 @@
+import { fetchFeaturedProducts } from '@/utils/actions';
 import SectionTitle from '../globals/SectionTitle';
+import EmptyList from '../globals/EmptyList';
+import ProductsGrid from '../products/ProductsGrid';
 
-function FeaturedProducts() {
+async function FeaturedProducts() {
+  const products = await fetchFeaturedProducts();
+
+  if (products.length === 0) return <EmptyList />;
+
   return (
     <div>
       <SectionTitle text="featured products" />
+      <ProductsGrid />
     </div>
   );
 }
