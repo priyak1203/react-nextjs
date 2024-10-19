e7b3c8dddaa4790c67ccaf846d92578f71efe35d
Priydarshini Karanth <kpriya1203@gmail.com>
2024-10-19 13:45:22 +0530

    navsearch component

--- 02-store-project/components/navbar/NavSearch.tsx
+++ 02-store-project/components/navbar/NavSearch.tsx
@@ -1,11 +1,45 @@
-import { Input } from '../ui/input';
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+import { Input } from '@/components/ui/input';
 
 const NavSearch = () => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+
+  const [search, setSearch] = useState(
+    searchParams.get('search')?.toString() || ''
+  );
+
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams);
+
+    if (value) {
+      params.set('search', value);
+    } else {
+      params.delete('search');
+    }
+    replace(`/products?${params.toString()}`);
+  }, 500);
+
+  useEffect(() => {
+    if (!searchParams.get('search')) {
+      setSearch('');
+    }
+  }, [searchParams.get('search')]);
+
   return (
     <Input
       type="search"
       placeholder="search product..."
       className="max-w-xs dark:bg-muted"
+      value={search}
+      onChange={(e) => {
+        setSearch(e.target.value);
+        handleSearch(e.target.value);
+      }}
     />
   );
 };
