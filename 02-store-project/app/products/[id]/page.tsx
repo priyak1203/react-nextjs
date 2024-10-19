5c81ed0cbbff39ee1c7967263a857262c663aee5
Priydarshini Karanth <kpriya1203@gmail.com>
2024-10-19 23:53:03 +0530

    single products page structure

--- 02-store-project/app/products/[id]/page.tsx
+++ 02-store-project/app/products/[id]/page.tsx
@@ -1,14 +1,48 @@
+import FavoriteToggleButton from '@/components/products/FavoriteToggleButton';
 import AddToCart from '@/components/single-product/AddToCart';
 import BreadCrumbs from '@/components/single-product/BreadCrumbs';
 import ProductRating from '@/components/single-product/ProductRating';
+import { fetchSingleProduct } from '@/utils/actions';
+import { formatCurrency } from '@/utils/format';
+import Image from 'next/image';
 
-function SingleProductPage() {
+async function SingleProductPage({ params }: { params: { id: string } }) {
+  const product = await fetchSingleProduct(params.id);
+  const { name, company, price, description, image } = product;
+  const dollarsAmount = formatCurrency(price);
+
   return (
-    <div>
-      <BreadCrumbs name="random" />
-      <ProductRating productId="123" />
-      <AddToCart productId="123" />
-    </div>
+    <section>
+      <BreadCrumbs name={name} />
+      <div className="mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16">
+        {/* IMAGE FIRST COL */}
+        <div className="relative h-full">
+          <Image
+            src={image}
+            alt={name}
+            fill
+            priority
+            sizes="(max-width:768px) 100vw, (max-width:1200px) 50vw, 33vw"
+            className="w-full rounded-md object-cover"
+          />
+        </div>
+
+        {/* PRODUCT INFO SECOND COL */}
+        <div>
+          <div className="flex gap-x-8 items-center">
+            <h1 className="capitalize text-3xl font-bold">{name}</h1>
+            <FavoriteToggleButton productId={params.id} />
+          </div>
+          <ProductRating productId={params.id} />
+          <h4 className="text-xl mt-2">{company}</h4>
+          <p className="mt-3 text-md bg-muted inline-block p-2 rounded-md">
+            {dollarsAmount}
+          </p>
+          <p className="mt-6 leading-8 text-muted-foreground">{description}</p>
+          <AddToCart productId={params.id} />
+        </div>
+      </div>
+    </section>
   );
 }
 
