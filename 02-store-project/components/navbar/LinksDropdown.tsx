7f45478bbbd31d14c81a0378a5f4618ad97934b3
Priydarshini Karanth <kpriya1203@gmail.com>
2024-10-21 15:28:46 +0530

    user icon component

--- 02-store-project/components/navbar/LinksDropdown.tsx
+++ 02-store-project/components/navbar/LinksDropdown.tsx
@@ -9,13 +9,15 @@ import { links } from '@/utils/links';
 import Link from 'next/link';
 import { LuAlignLeft } from 'react-icons/lu';
 import SignOutLink from './SignOutLink';
+import UserIcon from './UserIcon';
 
 function LinksDropdown() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" className="flex gap-4 max-w-[100px]">
+        <Button variant="outline" className="flex gap-4 max-w-[150px]">
           <LuAlignLeft className="h-6 w-6" />
+          <UserIcon />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-40" align="start" sideOffset={10}>
--- 02-store-project/components/navbar/UserIcon.tsx
+++ 02-store-project/components/navbar/UserIcon.tsx
@@ -1,4 +1,22 @@
-function UserIcon() {
-  return <div>UserIcon</div>;
+import { currentUser } from '@clerk/nextjs/server';
+import { LuUser2 } from 'react-icons/lu';
+
+async function UserIcon() {
+  const user = await currentUser();
+
+  const profileImage = user?.imageUrl;
+
+  if (profileImage) {
+    return (
+      <img
+        src={profileImage}
+        alt="avatar"
+        className="w-6 h-6 rounded-full object-cover"
+      />
+    );
+  }
+
+  return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white " />;
 }
+
 export default UserIcon;
