5c733c71eb83faf6fa4faad6770feeb14c8fc0b1
Priydarshini Karanth <kpriya1203@gmail.com>
2024-12-02 20:14:58 +0530

    rating and comment components

--- 02-store-project/components/reviews/Comment.tsx
+++ 02-store-project/components/reviews/Comment.tsx
@@ -1,4 +1,33 @@
-function Comment() {
-  return <div>Comment</div>;
+'use client';
+
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+
+function Comment({ comment }: { comment: string }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpanded = () => {
+    setIsExpanded((prev) => !prev);
+  };
+
+  const longComment = comment.length > 130;
+  const displayComment =
+    longComment && !isExpanded ? `${comment.slice(0, 130)}...` : comment;
+
+  return (
+    <div>
+      <p className="text-sm">{displayComment}</p>
+      {longComment && (
+        <Button
+          variant="link"
+          className="capitalize text-muted-foreground pl-0"
+          onClick={toggleExpanded}
+        >
+          {isExpanded ? 'show less' : 'show more'}
+        </Button>
+      )}
+    </div>
+  );
 }
+
 export default Comment;
--- 02-store-project/components/reviews/ProductReviews.tsx
+++ 02-store-project/components/reviews/ProductReviews.tsx
@@ -1,4 +1,16 @@
+import Comment from './Comment';
+import Rating from './Rating';
+
 function ProductReviews() {
-  return <div>ProductReviews</div>;
+  return (
+    <div>
+      <h2>product reviews</h2>
+      <Rating rating={3} />
+      <Comment
+        comment={`Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus delectus deleniti sit sapiente iusto consequuntur fuga facere eligendi alias eum nihil, autem dicta saepe nam voluptas repudiandae magni veniam nemo?`}
+      />
+    </div>
+  );
 }
+
 export default ProductReviews;
