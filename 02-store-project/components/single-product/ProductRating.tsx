a96b6cc245b7af7905eabdf2a7ac9fd17a702fc1
Priydarshini Karanth <kpriya1203@gmail.com>
2024-12-03 11:57:03 +0530

    product rating component dynamic rating calculation

--- 02-store-project/components/single-product/ProductRating.tsx
+++ 02-store-project/components/single-product/ProductRating.tsx
@@ -1,11 +1,11 @@
+import { fetchProductRating } from '@/utils/actions';
 import { FaStar } from 'react-icons/fa';
 
-function ProductRating({ productId }: { productId: string }) {
-  const rating = 4.2;
-  const count = 25;
+async function ProductRating({ productId }: { productId: string }) {
+  const { rating, count } = await fetchProductRating(productId);
   const countValue = `(${count}) reviews`;
-
   const className = `flex gap-1 items-center text-md mt-1 mb-4`;
+
   return (
     <span className={className}>
       <FaStar className="w-3 h-3" />
