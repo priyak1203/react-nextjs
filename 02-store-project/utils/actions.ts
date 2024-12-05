e85c53f7179b3f6105961fcdd2a77fbf2289cd1d
Priydarshini Karanth <kpriya1203@gmail.com>
2024-12-05 15:23:19 +0530

    add to cart action create cart item and update cart

--- 02-store-project/utils/actions.ts
+++ 02-store-project/utils/actions.ts
@@ -11,6 +11,7 @@ import {
 } from './schemas';
 import { deleteImage, uploadImage } from './supabase';
 import { revalidatePath } from 'next/cache';
+import { Cart } from '@prisma/client';
 
 const getAuthUser = async () => {
   const user = await currentUser();
@@ -435,6 +436,80 @@ export const fetchOrCreateCart = async ({
   return cart;
 };
 
+const updateOrCreateCartItem = async ({
+  productId,
+  cartId,
+  amount,
+}: {
+  productId: string;
+  cartId: string;
+  amount: number;
+}) => {
+  let cartItem = await db.cartItem.findFirst({
+    where: {
+      productId,
+      cartId,
+    },
+  });
+
+  if (cartItem) {
+    cartItem = await db.cartItem.update({
+      where: {
+        id: cartItem.id,
+      },
+      data: {
+        amount: cartItem.amount + amount,
+      },
+    });
+  } else {
+    cartItem = await db.cartItem.create({
+      data: {
+        amount,
+        productId,
+        cartId,
+      },
+    });
+  }
+};
+
+export const updateCart = async (cart: Cart) => {
+  const cartItems = await db.cartItem.findMany({
+    where: {
+      cartId: cart.id,
+    },
+    include: {
+      product: true,
+    },
+  });
+
+  let numItemsInCart = 0;
+  let cartTotal = 0;
+
+  for (const item of cartItems) {
+    numItemsInCart += item.amount;
+    cartTotal += item.amount * item.product.price;
+  }
+
+  const tax = cart.taxRate * cartTotal;
+  const shipping = cartTotal ? cart.shipping : 0;
+  const orderTotal = cartTotal + tax + shipping;
+
+  const currentCart = await db.cart.update({
+    where: {
+      id: cart.id,
+    },
+    data: {
+      numItemsInCart,
+      cartTotal,
+      tax,
+      orderTotal,
+    },
+    include: includeProductClause,
+  });
+
+  return currentCart;
+};
+
 export const addToCartAction = async (prevState: any, formData: FormData) => {
   const user = await getAuthUser();
 
@@ -443,7 +518,8 @@ export const addToCartAction = async (prevState: any, formData: FormData) => {
     const amount = Number(formData.get('amount'));
     await fetchProduct(productId);
     const cart = await fetchOrCreateCart({ userId: user.id });
-    console.log('cart', cart);
+    await updateOrCreateCartItem({ productId, cartId: cart.id, amount });
+    await updateCart(cart);
   } catch (error) {
     return renderError(error);
   }
