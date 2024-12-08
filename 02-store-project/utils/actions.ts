b5b5712867f4d7e479fd8bec0f6dd82ef7dc0385
Priydarshini Karanth <kpriya1203@gmail.com>
2024-12-08 20:41:55 +0530

    order actions

--- 02-store-project/utils/actions.ts
+++ 02-store-project/utils/actions.ts
@@ -589,6 +589,65 @@ export const updateCartItemAction = async ({
   }
 };
 
+// ======================================================== //
+//                  Order Action Functions                   //
+// ======================================================== //
+
 export const createOrderAction = async (prevState: any, formData: FormData) => {
-  return { message: 'order created' };
+  const user = await getAuthUser();
+  try {
+    const cart = await fetchOrCreateCart({
+      userId: user.id,
+      errorOnFailure: true,
+    });
+
+    const order = await db.order.create({
+      data: {
+        clerkId: user.id,
+        products: cart.numItemsInCart,
+        orderTotal: cart.orderTotal,
+        tax: cart.tax,
+        shipping: cart.shipping,
+        email: user.emailAddresses[0].emailAddress,
+      },
+    });
+
+    await db.cart.delete({
+      where: {
+        id: cart.id,
+      },
+    });
+  } catch (error) {
+    return renderError(error);
+  }
+  redirect('/orders');
+};
+
+export const fetchUserOrders = async () => {
+  const user = await getAuthUser();
+  const orders = await db.order.findMany({
+    where: {
+      clerkId: user.id,
+      isPaid: true,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
+  return orders;
+};
+
+export const fetchAdminOrders = async () => {
+  const user = await getAdminUser();
+  const orders = await db.order.findMany({
+    where: {
+      isPaid: true,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
+  return orders;
 };
