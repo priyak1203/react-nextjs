9626b882129e0e245e73bce9f23e5658c5cc07a2
Priydarshini Karanth <kpriya1203@gmail.com>
2024-12-09 14:57:01 +0530

    adding stripe dependencies and refactoring create order action and order schema

--- 02-store-project/utils/actions.ts
+++ 02-store-project/utils/actions.ts
@@ -595,10 +595,22 @@ export const updateCartItemAction = async ({
 
 export const createOrderAction = async (prevState: any, formData: FormData) => {
   const user = await getAuthUser();
+  let orderId: null | string = null;
+  let cartId: null | string = null;
+
   try {
     const cart = await fetchOrCreateCart({
       userId: user.id,
       errorOnFailure: true,
+    });
+
+    cartId = cart.id;
+
+    await db.order.deleteMany({
+      where: {
+        clerkId: user.id,
+        isPaid: false,
+      },
     });
 
     const order = await db.order.create({
@@ -612,15 +624,11 @@ export const createOrderAction = async (prevState: any, formData: FormData) => {
       },
     });
 
-    await db.cart.delete({
-      where: {
-        id: cart.id,
-      },
-    });
+    orderId = order.id;
   } catch (error) {
     return renderError(error);
   }
-  redirect('/orders');
+  redirect(`/checkout?orderId=${orderId}&cartId=${cartId}`);
 };
 
 export const fetchUserOrders = async () => {
