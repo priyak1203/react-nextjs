12c780882a2b320dc2011c542d3149eb2fefe2f0
Priydarshini Karanth <kpriya1203@gmail.com>
2024-12-09 21:01:02 +0530

    checkout page

---
+++ 02-store-project/app/checkout/page.tsx
@@ -0,0 +1,42 @@
+'use client';
+import axios from 'axios';
+import React, { useCallback } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import {
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout,
+} from '@stripe/react-stripe-js';
+import { useSearchParams } from 'next/navigation';
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
+);
+
+function CheckoutPage() {
+  const searchParams = useSearchParams();
+
+  const orderId = searchParams.get('orderId');
+  const cartId = searchParams.get('cartId');
+
+  const fetchClientSecret = useCallback(async () => {
+    // Create a Checkout Session
+    const response = await axios.post('/api/payment', {
+      orderId,
+      cartId,
+    });
+
+    return response.data.clientSecret;
+  }, []);
+
+  const options = { fetchClientSecret };
+
+  return (
+    <div id="checkout">
+      <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+        <EmbeddedCheckout />
+      </EmbeddedCheckoutProvider>
+    </div>
+  );
+}
+
+export default CheckoutPage;
