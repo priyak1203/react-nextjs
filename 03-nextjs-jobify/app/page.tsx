601f5fda4f524626adf60a300f8fcd64bdb09617
Priydarshini Karanth <kpriya1203@gmail.com>
2024-12-21 20:29:26 +0530

    setting up shadcn ui

--- 03-nextjs-jobify/app/page.tsx
+++ 03-nextjs-jobify/app/page.tsx
@@ -1,3 +1,14 @@
+import { Button } from '@/components/ui/button';
+import { Camera } from 'lucide-react';
+
 export default function HomePage() {
-  return <div>Jobify Project</div>;
+  return (
+    <div className="h-screen flex justify-center items-center gap-8">
+      <Button>Default Button</Button>
+      <Button variant="secondary">Secondary Button</Button>
+      <Button size="icon" variant="outline">
+        <Camera />
+      </Button>
+    </div>
+  );
 }
