7e580fae90919375a0555e2e465ebeb4ffb39263
Priydarshini Karanth <kpriya1203@gmail.com>
2024-12-22 20:52:55 +0530

    dashboard pages basic setup

---
+++ 03-nextjs-jobify/app/(dashboard)/add-job/page.tsx
@@ -0,0 +1,5 @@
+function AddJobPage() {
+  return <h1 className="text-4xl">AddJob Page</h1>;
+}
+
+export default AddJobPage;
---
+++ 03-nextjs-jobify/app/(dashboard)/stats/page.tsx
@@ -0,0 +1,5 @@
+function StatsPage() {
+  return <h1 className="text-4xl">Stats Page</h1>;
+}
+
+export default StatsPage;
