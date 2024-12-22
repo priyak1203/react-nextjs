7dcb855e09bad9f30cd3524e06d3b3462429f27a
Priydarshini Karanth <kpriya1203@gmail.com>
2024-12-22 20:41:14 +0530

    setup home page

--- 03-nextjs-jobify/app/page.tsx
+++ 03-nextjs-jobify/app/page.tsx
@@ -1,14 +1,37 @@
+import Image from 'next/image';
 import { Button } from '@/components/ui/button';
-import { Camera } from 'lucide-react';
+import logo from '../assets/logo.svg';
+import landingImg from '../assets/main.svg';
+import Link from 'next/link';
 
 export default function HomePage() {
   return (
-    <div className="h-screen flex justify-center items-center gap-8">
-      <Button>Default Button</Button>
-      <Button variant="secondary">Secondary Button</Button>
-      <Button size="icon" variant="outline">
-        <Camera />
-      </Button>
-    </div>
+    <main>
+      <header className="max-w-6xl mx-auto px-4 sm:px-8 py-6">
+        <Image src={logo} alt="logo" />
+      </header>
+      <section className="max-w-6xl mx-auto px-4 sm:px-8 h-screen -mt-20 grid lg:grid-cols-[1fr,400px] items-center">
+        <div>
+          <h1 className="capitalize text-4xl md:text-7xl font-bold">
+            job <span className="text-primary">tracking</span> app
+          </h1>
+          <p className="leading-loose max-w-md mt-4">
+            I am baby wayfarers hoodie next level taiyaki brooklyn cliche blue
+            bottle single-origin coffee chia. Aesthetic post-ironic venmo,
+            quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
+            narwhal.
+          </p>
+          <Button asChild className="mt-4">
+            <Link href="/add-job">Get Started</Link>
+          </Button>
+        </div>
+
+        <Image
+          src={landingImg}
+          alt="landing image"
+          className="hidden lg:block"
+        />
+      </section>
+    </main>
   );
 }
