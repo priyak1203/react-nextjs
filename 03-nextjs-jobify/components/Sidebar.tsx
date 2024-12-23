bd54888317b81a832344575a697f8cc3940bcc2c
Priydarshini Karanth <kpriya1203@gmail.com>
2024-12-23 12:41:27 +0530

    sidebar component

--- 03-nextjs-jobify/components/Sidebar.tsx
+++ 03-nextjs-jobify/components/Sidebar.tsx
@@ -1,5 +1,36 @@
+'use client';
+
+import Image from 'next/image';
+import logo from '../assets/logo.svg';
+import links from '@/utils/links';
+import Link from 'next/link';
+import { Button } from './ui/button';
+import { usePathname } from 'next/navigation';
+
 function Sidebar() {
-  return <div className="border-blue-700 border-2">Sidebar</div>;
+  const pathname = usePathname();
+
+  return (
+    <aside className="py-4 px-8 bg-muted h-full">
+      <Image src={logo} alt="logo" className="mx-auto" />
+      <div className="flex flex-col mt-20 gap-y-4">
+        {links.map((link) => {
+          const { href, label, icon } = link;
+          return (
+            <Button
+              asChild
+              key={href}
+              variant={pathname === href ? 'default' : 'link'}
+            >
+              <Link href={href} className="flex items-center gap-x-2">
+                {icon} <span className="capitalize">{label}</span>
+              </Link>
+            </Button>
+          );
+        })}
+      </div>
+    </aside>
+  );
 }
 
 export default Sidebar;
