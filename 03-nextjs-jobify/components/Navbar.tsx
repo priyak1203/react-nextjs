70193fc02c9ae477f3549245f22925d65e060f30
Priydarshini Karanth <kpriya1203@gmail.com>
2024-12-24 18:45:45 +0530

    navbar basic structure

--- 03-nextjs-jobify/components/Navbar.tsx
+++ 03-nextjs-jobify/components/Navbar.tsx
@@ -1,5 +1,19 @@
+import { UserButton } from '@clerk/nextjs';
+import LinksDropdown from './LinksDropdown';
+import ThemeToggle from './ThemeToggle';
+
 function Navbar() {
-  return <div className="border-red-500 border">Navbar</div>;
+  return (
+    <nav className="bg-muted py-4 px-4 sm:px-16 lg:px-24 flex items-center justify-between ">
+      <div>
+        <LinksDropdown />
+      </div>
+      <div className="flex items-center gap-x-4">
+        <ThemeToggle />
+        <UserButton />
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
