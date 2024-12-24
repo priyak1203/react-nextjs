eace325aeb02ff1ba5abe0ba53e5ae626326b004
Priydarshini Karanth <kpriya1203@gmail.com>
2024-12-24 23:20:54 +0530

    links dropdown component

--- 03-nextjs-jobify/components/LinksDropdown.tsx
+++ 03-nextjs-jobify/components/LinksDropdown.tsx
@@ -1,5 +1,41 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
+import { AlignLeft } from 'lucide-react';
+import links from '@/utils/links';
+import Link from 'next/link';
+
 function LinksDropdown() {
-  return <div>LinksDropdown</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="lg:hidden">
+        <Button variant="outline" size="icon">
+          <AlignLeft />
+          <span className="sr-only">Toggle links</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-52 lg:hidden"
+        align="start"
+        sideOffset={25}
+      >
+        {links.map((link) => {
+          const { icon, href, label } = link;
+          return (
+            <DropdownMenuItem key={href}>
+              <Link href={href} className="flex items-center gap-x-2">
+                {icon} <span className="capitalize">{label}</span>
+              </Link>
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
 
 export default LinksDropdown;
