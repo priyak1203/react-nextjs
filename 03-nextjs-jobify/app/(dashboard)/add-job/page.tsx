3b2565c7996c6baa72c44fc32d80c3220ac04dc5
Priydarshini Karanth <kpriya1203@gmail.com>
2024-12-26 13:49:26 +0530

    create job form basic structure and setup

--- 03-nextjs-jobify/app/(dashboard)/add-job/page.tsx
+++ 03-nextjs-jobify/app/(dashboard)/add-job/page.tsx
@@ -1,5 +1,12 @@
+import CreateJobForm from '@/components/CreateJobForm';
+
 function AddJobPage() {
-  return <h1 className="text-4xl">AddJob Page</h1>;
+  return (
+    <div>
+      <h1 className="text-4xl mb-4">AddJob Page</h1>
+      <CreateJobForm />
+    </div>
+  );
 }
 
 export default AddJobPage;
