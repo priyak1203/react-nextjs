de4ec0cb79c8a76267c825a6fabce59c15fc21ad
Priydarshini Karanth <kpriya1203@gmail.com>
2024-12-29 11:55:13 +0530

    Create Job Form with custom form components

--- 03-nextjs-jobify/app/(dashboard)/add-job/page.tsx
+++ 03-nextjs-jobify/app/(dashboard)/add-job/page.tsx
@@ -2,10 +2,9 @@ import CreateJobForm from '@/components/CreateJobForm';
 
 function AddJobPage() {
   return (
-    <div>
-      <h1 className="text-4xl mb-4">AddJob Page</h1>
+    <>
       <CreateJobForm />
-    </div>
+    </>
   );
 }
 
--- 03-nextjs-jobify/components/FormComponents.tsx
+++ 03-nextjs-jobify/components/FormComponents.tsx
@@ -31,6 +31,7 @@ export function CustomFormField({ name, control }: CustomFormFieldProps) {
           <FormControl>
             <Input {...field} />
           </FormControl>
+          <FormMessage />
         </FormItem>
       )}
     />
