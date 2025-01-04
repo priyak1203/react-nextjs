0bc49aa268b596865757a360e73b2b2aaa3156a5
Priydarshini Karanth <kpriya1203@gmail.com>
2025-01-04 12:13:39 +0530

    jobs list basic setup

--- 03-nextjs-jobify/components/JobCard.tsx
+++ 03-nextjs-jobify/components/JobCard.tsx
@@ -1,4 +1,6 @@
-function JobCard() {
+import { JobType } from '@/utils/types';
+
+function JobCard({ job }: { job: JobType }) {
   return <div>JobCard</div>;
 }
 export default JobCard;
--- 03-nextjs-jobify/components/JobsList.tsx
+++ 03-nextjs-jobify/components/JobsList.tsx
@@ -1,5 +1,38 @@
+'use client';
+
+import { getAllJobs } from '@/utils/actions';
+import { useQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'next/navigation';
+import JobCard from './JobCard';
+
 function JobsList() {
-  return <div>JobsList</div>;
+  const searchParams = useSearchParams();
+  const search = searchParams.get('search') || '';
+  const jobStatus = searchParams.get('jobStatus') || 'all';
+  const pageNumber = Number(searchParams.get('page')) || 1;
+
+  const { data, isPending } = useQuery({
+    queryKey: ['jobs', search ?? '', jobStatus, pageNumber],
+    queryFn: () => getAllJobs({ search, jobStatus, page: pageNumber }),
+  });
+
+  const jobs = data?.jobs || [];
+
+  console.log(jobs);
+
+  if (isPending) return <h2 className="text-xl">Please Wait...</h2>;
+
+  if (jobs.length < 1) return <h2 className="text-xl">No Jobs Found...</h2>;
+
+  return (
+    <>
+      <div>
+        {jobs.map((job) => {
+          return <JobCard key={job.id} job={job} />;
+        })}
+      </div>
+    </>
+  );
 }
 
 export default JobsList;
