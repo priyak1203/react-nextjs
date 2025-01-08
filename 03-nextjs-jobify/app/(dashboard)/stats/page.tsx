247dadef56820e164644946b17b9a84795962ca6
Priydarshini Karanth <kpriya1203@gmail.com>
2025-01-08 23:29:48 +0530

    getstatsaction function

--- 03-nextjs-jobify/app/(dashboard)/stats/page.tsx
+++ 03-nextjs-jobify/app/(dashboard)/stats/page.tsx
@@ -1,4 +1,9 @@
-function StatsPage() {
+import { getStatsAction } from '@/utils/actions';
+
+async function StatsPage() {
+  const result = await getStatsAction();
+  console.log(result);
+
   return <h1 className="text-4xl">Stats Page</h1>;
 }
 
